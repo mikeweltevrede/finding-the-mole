9df272f28dadac15e771d7b260ed2f4927af587c
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2023-11-11 19:40:45 +0100

    Generalize data extractor wrapper

--- tests/unit/shared/data_extraction_test.py
+++ tests/unit/shared/data_extraction_test.py
@@ -12,7 +12,7 @@ from tests.utils import io_utils
 class TestDataExtractor:
     @pytest.fixture()
     def data_extractor(self) -> DataExtractor:
-        return DataExtractor()
+        return DataExtractor(io_library=pl.io)
 
     def test_data_extractor_load_csv_outputs_correct_polars_df(self, data_extractor: DataExtractor):
         data = {"col1": list(range(3)), "col2": list(range(3))}
@@ -21,6 +21,6 @@ class TestDataExtractor:
         with tempfile.TemporaryDirectory() as tmp_dir:
             data_path = Path(tmp_dir) / "data.csv"
             io_utils.write_dict_to_csv(data=data, path=data_path)
-            actual = data_extractor.read_csv(path=data_path)
+            actual = data_extractor.io.read_csv(data_path)
 
         pl_testing.assert_frame_equal(actual, expected)
