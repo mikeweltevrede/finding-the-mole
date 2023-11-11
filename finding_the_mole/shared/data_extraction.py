d5bb52dfdfa2f3e27c3b2d5f45785a4764ee71ad
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2023-11-11 20:48:23 +0100

    Remove docstring from class - use init instead

--- finding_the_mole/shared/data_extraction.py
+++ finding_the_mole/shared/data_extraction.py
@@ -4,10 +4,8 @@ import polars as pl
 
 
 class DataExtractor:
-    """Data Extractor to read data using an input IO library."""
-
     def __init__(self, io_library: ModuleType = None) -> None:
-        """Initialize DataExtractor.
+        """Data Extractor to read data using an input IO library.
 
         The `io_library` argument can be set to determine in which way to load data. Load data by calling
         `self.io.<desired_load_function>`.
