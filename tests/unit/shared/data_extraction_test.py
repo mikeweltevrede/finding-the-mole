705b4daa26a6f1b00ab9fa94db7d03c2c2a988c7
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2023-11-11 20:55:42 +0100

    Tests don't need to check written file anymore

--- tests/unit/shared/data_extraction_test.py
+++ tests/unit/shared/data_extraction_test.py
@@ -1,26 +1,12 @@
-import tempfile
-from pathlib import Path
-
 import polars as pl
-import pytest
-from polars import testing as pl_testing
+from assertpy import assert_that
 
 from finding_the_mole.shared.data_extraction import DataExtractor
-from tests.utils import io_utils
 
 
 class TestDataExtractor:
-    @pytest.fixture()
-    def data_extractor(self) -> DataExtractor:
-        return DataExtractor(io_library=pl.io)
+    def test_data_extractor_io_defaults_to_polars_io(self):
+        assert_that(DataExtractor().io).is_equal_to(pl.io)
 
-    def test_data_extractor_load_csv_outputs_correct_polars_df(self, data_extractor: DataExtractor):
-        data = {"col1": list(range(3)), "col2": list(range(3))}
-        expected = pl.DataFrame(data)
-
-        with tempfile.TemporaryDirectory() as tmp_dir:
-            data_path = Path(tmp_dir) / "data.csv"
-            io_utils.write_dict_to_csv(data=data, path=data_path)
-            actual = data_extractor.io.read_csv(data_path)
-
-        pl_testing.assert_frame_equal(actual, expected)
+    def test_data_extractor_io_equals_polars_io_when_io_library_is_none(self):
+        assert_that(DataExtractor(io_library=None).io).is_equal_to(pl.io)
