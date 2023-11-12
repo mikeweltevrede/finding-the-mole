ff5aa3ef094094dbe7d17b001a6c58a8021947d5
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2023-11-12 14:33:38 +0100

    Fit method for baseline model

--- tests/unit/baseline_model/model_test.py
+++ tests/unit/baseline_model/model_test.py
@@ -1,4 +1,5 @@
 import polars as pl
+import polars.testing as pl_testing
 import pytest
 from assertpy import assert_that
 
@@ -14,3 +15,15 @@ class TestBaselineModel:
         baseline_model.fit(data=pl.DataFrame())
 
         assert_that(baseline_model._fitted).is_true()
+
+    def test_fit_computes_count_of_m_per_row(self, baseline_model: BaselineModel):
+        data = pl.DataFrame(
+            data=[
+                dict(Name="John", Task1="M", Task2="M", Task3="O"),
+                dict(Name="Mary", Task1="P", Task2="M", Task3="O"),
+            ]
+        )
+        expected = pl.DataFrame(data=[dict(Name="John", count=2.0), dict(Name="Mary", count=1.0)])
+
+        fitted_model = baseline_model.fit(data=data, index_col="Name")
+        pl_testing.assert_frame_equal(fitted_model.counts, expected)
