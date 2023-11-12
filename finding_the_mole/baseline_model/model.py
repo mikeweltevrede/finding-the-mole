1f7e7c25415c22b7670ce811cc24dff0f8fbd135
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2023-11-12 14:33:38 +0100

    Fit method for baseline model

--- finding_the_mole/baseline_model/model.py
+++ finding_the_mole/baseline_model/model.py
@@ -13,8 +13,9 @@ class BaselineModel(Model):
         The predicted score will be the probability of that count compared to the total count.
         """
         Model.__init__(self)
+        self.counts = None
 
-    def fit(self: Self, data: pl.DataFrame) -> Self:
+    def fit(self: Self, data: pl.DataFrame, index_col: str = None) -> Self:
         """Method to fit model on data.
 
         It will count the number of times that a candidate has been in a role in which I would expect the Mole to be in.
@@ -22,12 +23,20 @@ class BaselineModel(Model):
         Args:
             data: Input data to fit the model on. Index represents the candidate name or index and columns are
                 indicating which role the candidate was in.
+            index_col: Column name which represents an index to compute the count over.
 
         Returns:
             Fitted model.
         """
+        # TODO: Refactor to separate function in data preprocessing
+        mapper = {"M": 1, "P": 0, "X": -1e6, "O": 0}
+        data = data.select(index_col, pl.all().exclude(index_col).map_dict(mapper))
+
+        counts = data.with_columns(data.select(pl.all().exclude(index_col)).sum(axis=1).alias("count"))
+        self.counts = counts.select(index_col, "count")
         self._fitted = True
-        print(data)  # temp to avoid PCH trigger
+
+        return self
 
     def predict(self, data: pl.DataFrame) -> list[float]:
         """Method to predict on data with the model.
