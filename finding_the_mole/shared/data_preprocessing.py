697ae22bc05de4c22c46c551fbcc0af76e83bf8e
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2023-11-18 16:10:01 +0100

    limit_data_to_train_set for data preprocessing

--- finding_the_mole/shared/data_preprocessing.py
+++ finding_the_mole/shared/data_preprocessing.py
@@ -1,11 +1,31 @@
+from __future__ import annotations
+
 import math
-from typing import Any, Hashable, Iterable
+from dataclasses import dataclass
+from typing import Any, Hashable, Iterable, Literal
 
 import polars as pl
+from dataclass_wizard import YAMLWizard
 
 
 class DataPreprocessor:
     MAPPER = {"M": 1, "O": 0, "P": 0, "X": -math.inf}
+
+    @dataclass
+    class Context(YAMLWizard):
+        """Context dataclass for data preprocessing"""
+
+        index_col: str
+        num_episodes_to_train_on: int | Literal["all"]
+        tasks_per_episode: int = 3
+
+    def __init__(self, context: DataPreprocessor.Context) -> None:
+        """Data preprocessing class
+
+        Args:
+            context: DataPreprocessor context dataclass with configuration values.
+        """
+        self.context = context
 
     @classmethod
     def extend_mapper_str_keys_with_upper_and_lowercase(cls, mapper: dict[str, Any]) -> dict[str, Any]:
@@ -46,3 +66,22 @@ class DataPreprocessor:
         if exclude_cols:
             return data.select(*exclude_cols, pl.all().exclude(*exclude_cols).map_dict(mapper))
         return data.select(pl.all().map_dict(mapper))
+
+    def limit_data_to_train_set(self, data: pl.DataFrame, prefix_task_cols: str = "Task") -> pl.DataFrame:
+        """Filters columns in `data` to only keep the task columns to train on, keeping `self.context.index_col`.
+
+        Using `self.context.tasks_per_episode` and `self.context.num_episodes_to_train_on`, we determine which columns
+        to keep. If `self.context.num_episodes_to_train_on` is `"all"`, we don't need to filter anything.
+
+        Args:
+            data: Data to filter columns to train set for.
+            prefix_task_cols: Prefix for the task columns. Combined with an integer to determine the column names.
+
+        Returns:
+            Data with only the columns of the train set, including the index column.
+        """
+        if self.context.num_episodes_to_train_on == "all":
+            return data
+
+        tasks_to_keep = range(1, self.context.num_episodes_to_train_on * self.context.tasks_per_episode + 1)
+        return data.select(self.context.index_col, *(f"{prefix_task_cols}{num}" for num in tasks_to_keep))
