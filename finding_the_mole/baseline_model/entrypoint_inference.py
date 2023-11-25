2d97511574e469cae7c97bd1ab05c5c23534f72b
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2023-11-25 16:05:15 +0100

    Merge training and inference for baseline model

--- finding_the_mole/baseline_model/entrypoint_inference.py
+++ finding_the_mole/baseline_model/entrypoint_inference.py
@@ -8,7 +8,8 @@ from dataclass_wizard import YAMLWizard
 
 from finding_the_mole.baseline_model.model import BaselineModel
 from finding_the_mole.shared.abstract_entrypoint_inference import AbstractInferenceJob
-from finding_the_mole.shared.abstract_model import Model
+from finding_the_mole.shared.data_extraction import DataExtractor
+from finding_the_mole.shared.data_preprocessing import DataPreprocessor
 
 
 class InferenceJob(AbstractInferenceJob):
@@ -18,8 +19,11 @@ class InferenceJob(AbstractInferenceJob):
     class Context(YAMLWizard):
         """Context dataclass for inference job"""
 
-        read_path_models: str
+        read_path: str
+        data_file_name: str
+        write_path_models: str
         model_pickle_name: str
+        index_col: str
 
     def __init__(self, config_path: str) -> None:
         """Entrypoint to the training job.
@@ -33,17 +37,40 @@ class InferenceJob(AbstractInferenceJob):
 
     def launch(self) -> None:
         """Main method of the InferenceJob. Orchestrates all other logic."""
-        model = self.model_loading()
+        data = self.data_extraction()
+        data_preprocessed = self.data_preprocessing(data=data)
+        model = self.model_training(data=data_preprocessed)
         results = self.model_inference(model=model)
 
         # Show the results for all participants that are not eliminated, sorted from highest to lowest score
         print(results.filter(pl.col("count") != -math.inf).sort(self.SCORE_COL, descending=True))
 
-    def model_loading(self) -> Model:
-        """Model loading orchestration method of the InferenceJob."""
-        return BaselineModel.from_pickle(
-            path=Path(self.context.read_path_models) / "BaselineModel" / self.context.model_pickle_name
+    def data_extraction(self) -> pl.DataFrame:
+        """Data extraction orchestration method of the TrainingJob."""
+        data_extractor = DataExtractor()
+        return data_extractor.io.read_csv(source=Path(self.context.read_path) / self.context.data_file_name)
+
+    def data_preprocessing(self, data: pl.DataFrame) -> pl.DataFrame:
+        """Data preprocessing orchestration method of the TrainingJob."""
+        context = DataPreprocessor.Context.from_yaml_file(file=self.config_path, decoder=yaml.safe_load)
+        data_preprocessor = DataPreprocessor(context=context)
+
+        mapper = data_preprocessor.extend_mapper_str_keys_with_upper_and_lowercase(mapper=data_preprocessor.MAPPER)
+        data_prepped = data_preprocessor.map_values(data=data, mapper=mapper, exclude_cols=[self.context.index_col])
+        return data_preprocessor.limit_data_to_train_set(data=data_prepped)
+
+    def model_training(self, data: pl.DataFrame) -> BaselineModel:
+        """Model training orchestration method of the TrainingJob.
+
+        TODO: Store which data it was trained on (which tasks/episodes) so that we can partition the training and
+            inference output based on this.
+        """
+        model = BaselineModel()
+        fitted_model = model.fit(data=data, exclude_cols=[self.context.index_col])
+        fitted_model.to_pickle(
+            path=Path(self.context.write_path_models) / fitted_model.__class__.__name__ / self.context.model_pickle_name
         )
+        return fitted_model
 
     def model_inference(self, **kwargs) -> pl.DataFrame:
         """Model inference orchestration method of the InferenceJob.
